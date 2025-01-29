9fa632d5770e85f0b8f387dfc7f6c1959b3abd92
rychames <rychames@gmail.com>
2025-01-29 07:07:26 -0400

    Atualização da Page de Produtos

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -32,9 +32,12 @@ export default function Header() {
       <nav className="bg-[#004022]">
         <div className="max-w-screen-xl flex flex-col lg:flex-row items-center justify-between mx-auto p-4">
           <div className="flex items-center space-x-8 mb-4 lg:mb-0">
-            <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-              <img src="/ppscanner.svg" className="h-[4rem] w-auto" alt="Logo" />
-            </a>
+            <ProtectedLink href='/'>
+
+                <img src="/ppscanner.svg" className="h-[4rem] w-auto" alt="Logo" />
+
+            </ProtectedLink>
+
             <button
               onClick={toggleMenu}
               type="button"
@@ -56,17 +59,17 @@ export default function Header() {
           >
             <ul className="font-medium flex flex-col p-4 md:flex-row md:space-x-8 w-full md:w-auto border border-gray-100 rounded-lg bg-[#004022] md:mt-0 md:border-0 md:bg-transparent">
               <li className="flex-1 md:flex-none">
-                <ProtectedLink href="/">
+                <ProtectedLink href="/add-product">
                   <div className="group flex items-center justify-center py-3 px-4 text-white hover:text-gray-300 relative">
-                    Home
+                    Novo Produto
                     <span className="absolute bottom-0 left-0 right-0 h-1 bg-gradient-to-r from-[#004022] to-white scale-x-0 group-hover:scale-x-100 transition-transform origin-left"></span>
                   </div>
                 </ProtectedLink>
               </li>
               <li className="flex-1 md:flex-none">
-                <ProtectedLink href="/add-product">
+                <ProtectedLink href="/">
                   <div className="group flex items-center justify-center py-3 px-4 text-white hover:text-gray-300 relative">
-                    Novo Produto
+                    Gerencia dos produtos
                     <span className="absolute bottom-0 left-0 right-0 h-1 bg-gradient-to-r from-[#004022] to-white scale-x-0 group-hover:scale-x-100 transition-transform origin-left"></span>
                   </div>
                 </ProtectedLink>
@@ -82,26 +85,28 @@ export default function Header() {
               {token ? (
                 <li className="flex-1 md:flex-none relative">
                   {/* Ícone de usuário com dropdown */}
-                  <button
-                    onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-                    className="flex items-center py-3 px-4 text-white hover:text-gray-300"
-                  >
-                    <User className="w-5 h-5 mr-2" />
-                    Perfil
-                  </button>
-                  {isDropdownOpen && (
-                    <ul className="absolute bg-white text-black rounded-md shadow-lg right-0 mt-2 py-2 w-40 z-50">
-                      <li>
-                        <button
-                          onClick={handleLogout}
-                          className="block px-4 py-2 text-sm hover:bg-gray-200 w-full text-left"
-                        >
-                          <LogOut className="w-4 h-4 inline mr-2" />
-                          Sair
-                        </button>
-                      </li>
-                    </ul>
-                  )}
+                  <div className="relative">
+                    <button
+                      onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                      className="flex items-center py-3 px-4 text-white hover:text-gray-300"
+                    >
+                      <User className="w-5 h-5 mr-2" />
+                      Perfil
+                    </button>
+                    {isDropdownOpen && (
+                      <ul className="absolute bg-white text-black rounded-md shadow-lg right-0 mt-2 py-2 w-40 z-50">
+                        <li>
+                          <button
+                            onClick={handleLogout}
+                            className="block px-4 py-2 text-sm hover:bg-gray-200 w-full text-left"
+                          >
+                            <LogOut className="w-4 h-4 inline mr-2" />
+                            Sair
+                          </button>
+                        </li>
+                      </ul>
+                    )}
+                  </div>
                 </li>
               ) : (
                 <li className="flex-1 md:flex-none">
---
+++ src/app/components/formPageOthers.tsx
@@ -0,0 +1,330 @@
+"use client";
+
+import { useState } from "react";
+import Swal from "sweetalert2";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import {
+    FaUser,
+    FaTag,
+    FaClipboard,
+    FaBox,
+    FaCheckSquare,
+    FaClock,
+    FaPen,
+    FaWeight,
+    FaImage,
+    FaPlus,
+} from "react-icons/fa";
+
+export default function FormPageOthers() {
+    const [formData, setFormData] = useState({
+        name: "",
+        category: "",
+        model: "",
+        brand: "",
+        description: "",
+        quantity: "",
+        size: "",
+        unitOrBox: false,
+        deliveredBy: "",
+        receivedBy: "",
+        deliveryTime: "",
+    });
+
+    const [images, setImages] = useState<File[]>([]);
+    const [previewImages, setPreviewImages] = useState<string[]>([]);
+    const router = useRouter();
+
+    const categories = ["Eletrônicos", "Roupas", "Alimentos"]; // Exemplo de categorias
+
+    const handleInputChange = (
+        e: React.ChangeEvent<
+            HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+        >
+    ) => {
+        const { name, value, type } = e.target;
+
+        setFormData({
+            ...formData,
+            [name]:
+                type === "checkbox"
+                    ? (e.target as HTMLInputElement).checked
+                        ? 1
+                        : 0
+                    : value,
+        });
+    };
+
+    const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (!e.target.files) return;
+
+        const files = Array.from(e.target.files);
+        setImages(files);
+
+        const previews = files.map((file) => URL.createObjectURL(file));
+        setPreviewImages(previews);
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const form = new FormData();
+        Object.entries(formData).forEach(([key, value]) => {
+            form.append(key, String(value));
+        });
+
+        images.forEach((image) => {
+            form.append("images", image); // "images" será a chave usada no backend
+        });
+
+        // Logando os dados enviados no console
+        console.log("Dados do Formulário:");
+        Object.entries(formData).forEach(([key, value]) => {
+            console.log(`${key}: ${value}`);
+        });
+
+        console.log("Imagens:");
+        images.forEach((image, index) => {
+            console.log(`Imagem ${index + 1}:`, image.name);
+        });
+
+        try {
+            const response = await axios.post(
+                "http://10.0.0.173:8000/api/inventory/",
+                form,
+                {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    },
+                }
+            );
+
+            console.log("Resposta do Servidor:", response.data);
+
+            if (response.status === 200) {
+                Swal.fire("Sucesso", "Produto cadastrado com sucesso!", "success");
+                router.push("/produtos");
+            }
+        } catch (error) {
+            console.error("Erro ao enviar o formulário:", error);
+            Swal.fire("Erro", "Falha ao cadastrar o produto.", "error");
+        }
+    };
+
+
+    return (
+        <div className="p-6 md:p-12 bg-gray-100 min-h-screen relative z-10">
+            <div className="max-w-4xl mx-auto bg-white shadow-xl rounded-lg p-8">
+                <h1 className="text-4xl font-bold mb-8 text-gray-800 text-center">
+                    Inventário outras Empresas
+                </h1>
+                <form onSubmit={handleSubmit} className="space-y-6">
+                    {/* Nome e Categoria */}
+                    <div className="space-y-4">
+                        <div className="flex items-center space-x-3">
+                            <FaUser className="text-green-500" />
+                            <input
+                                name="name"
+                                type="text"
+                                placeholder="Nome do Produto"
+                                className="w-full border border-gray-300 p-3 rounded-lg focus:ring focus:ring-green-300"
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div>
+                        <div className="flex items-center space-x-3">
+                            <FaTag className="text-green-500" />
+                            <select
+                                name="category"
+                                className="w-full border border-gray-300 p-3 rounded-lg focus:ring focus:ring-green-300"
+                                onChange={handleInputChange}
+                                required
+                            >
+                                <option value="">Selecione uma Categoria</option>
+                                {categories.map((cat) => (
+                                    <option key={cat} value={cat}>
+                                        {cat}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                    </div>
+
+                    {/* Modelo e Marca */}
+                    <div className="space-y-4">
+                        <div className="flex items-center space-x-3">
+                            <FaClipboard className="text-green-500" />
+                            <input
+                                name="model"
+                                type="text"
+                                placeholder="Modelo do Produto"
+                                className="w-full border border-gray-300 p-3 rounded-lg focus:ring focus:ring-green-300"
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div>
+                        <div className="flex items-center space-x-3">
+                            <FaBox className="text-green-500" />
+                            <input
+                                name="brand"
+                                type="text"
+                                placeholder="Marca"
+                                className="w-full border border-gray-300 p-3 rounded-lg focus:ring focus:ring-green-300"
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div>
+                    </div>
+
+                    {/* Descrição */}
+                    <div className="space-y-4">
+                        <div className="flex items-center space-x-3">
+                            <FaPen className="text-green-500" />
+                            <textarea
+                                name="description"
+                                placeholder="Descrição do Produto"
+                                className="w-full border border-gray-300 p-3 rounded-lg focus:ring focus:ring-green-300"
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div>
+                    </div>
+
+                    {/* Quantidade e Tamanho */}
+                    <div className="space-y-4">
+                        <div className="flex items-center space-x-3">
+                            <FaWeight className="text-green-500" />
+                            <input
+                                name="quantity"
+                                type="number"
+                                placeholder="Quantidade"
+                                className="w-full border border-gray-300 p-3 rounded-lg focus:ring focus:ring-green-300"
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div>
+                        <div className="flex items-center space-x-3">
+                            <FaTag className="text-green-500" />
+                            <select
+                                name="size"
+                                className="w-full border border-gray-300 p-3 rounded-lg focus:ring focus:ring-green-300"
+                                onChange={handleInputChange}
+                                required
+                            >
+                                <option value="">Selecione o Tamanho</option>
+                                <option value="P">P</option>
+                                <option value="M">M</option>
+                                <option value="G">G</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    {/* CheckBox */}
+                    <div className="flex items-center space-x-3">
+                        <FaCheckSquare className="text-green-500" />
+                        <input
+                            name="unitOrBox"
+                            type="checkbox"
+                            id="unitOrBox"
+                            className="h-5 w-5 text-green-500 border-gray-300 rounded focus:ring-green-300"
+                            onChange={handleInputChange}
+                        />
+                        <label htmlFor="unitOrBox" className="text-gray-700">
+                            Por Caixa (desmarcar para Unidade)
+                        </label>
+                    </div>
+
+                    {/* Entrega */}
+                    <div className="space-y-4">
+                        <div className="flex items-center space-x-3">
+                            <FaUser className="text-green-500" />
+                            <input
+                                name="deliveredBy"
+                                type="text"
+                                placeholder="Entregue Por"
+                                className="w-full border border-gray-300 p-3 rounded-lg focus:ring focus:ring-green-300"
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div>
+                        <div className="flex items-center space-x-3">
+                            <FaUser className="text-green-500" />
+                            <input
+                                name="receivedBy"
+                                type="text"
+                                placeholder="Recebido Por"
+                                className="w-full border border-gray-300 p-3 rounded-lg focus:ring focus:ring-green-300"
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div>
+                        <div className="flex items-center space-x-3">
+                            <FaClock className="text-green-500" />
+                            <input
+                                name="deliveryTime"
+                                type="text"
+                                placeholder="Horário de Entrega"
+                                value={formData.deliveryTime}
+                                className="w-full border border-gray-300 p-3 rounded-lg focus:ring focus:ring-green-300"
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                    </div>
+
+                    {/* Upload de Imagens */}
+                    <div className="space-y-4">
+                        <label className="text-gray-700 font-bold mb-2 flex items-center space-x-2">
+                            <FaImage className="text-green-500" />
+                            <span>Imagens do Produto</span>
+                        </label>
+                        <div className="border border-gray-300 p-4 rounded-lg bg-gray-50">
+                            <div className="flex items-center justify-between">
+                                <p className="text-gray-500 text-sm">
+                                    Você pode enviar até 5 imagens.
+                                </p>
+                                <label
+                                    htmlFor="image-upload"
+                                    className="cursor-pointer bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 flex items-center space-x-2"
+                                >
+                                    <FaPlus className="text-white" />
+                                    <span>Adicionar Imagem</span>
+                                </label>
+                                <input
+                                    type="file"
+                                    name="images"
+                                    multiple
+                                    accept="image/*"
+                                    className="hidden"
+                                    id="image-upload"
+                                    onChange={handleImageUpload}
+                                />
+                            </div>
+                            <div className="mt-4 grid grid-cols-2 md:grid-cols-3 gap-4">
+                                {previewImages.map((src, index) => (
+                                    <div
+                                        key={index}
+                                        className="relative rounded-lg overflow-hidden shadow-lg"
+                                    >
+                                        <img
+                                            src={src}
+                                            alt={`Prévia ${index + 1}`}
+                                            className="w-full h-full object-cover"
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="w-full bg-green-500 text-white py-3 rounded-lg hover:bg-green-600 focus:outline-none focus:ring focus:ring-green-300"
+                    >
+                        Cadastrar Produto
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}
